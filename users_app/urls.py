672321f4a15d786d2fd4506665d165c9511af960
alifareeq77 <alifareeq77@gmail.com>
2023-11-23 20:06:43 +0300

    fixed typo

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,13 +1,14 @@
 from django.template.defaulttags import url
 from django.urls import include, re_path, path
 from rest_framework import routers
-from form_app.views import FormViewSet
+from form_app.views import FormViewSet, FormAppliersViewSet
 from users_app.views import ActivateUser, AppliersViewSet, OwnerViewSet
 
 router = routers.SimpleRouter()
 router.register(r'auth/users/appliers', AppliersViewSet, basename='applier create')
 router.register(r'auth/users/owners', OwnerViewSet, basename='applier create')
-router.register(r'form',F)
+router.register(r'form_appliers', FormAppliersViewSet)
+router.register(r'form', FormViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     re_path(r'^auth/', include('djoser.urls')),
